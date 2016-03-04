747f327c6f57aae279ef581b19f57c0603bf3eeb
System Administrator <root@Juans-MacBook-Air.local>
2016-03-04 16:51:44 -0700

    added resources

---
+++ resources/views/emails/verification.blade.php
@@ -0,0 +1,6 @@
+<h2>Hi, {{$name}}</h2>
+<br>
+<p>
+	Please confirm your account, click on this link 
+	<a href="http://clubmein.app/access/confirm/{{$key}}">http://clubmein.app/confirmation?code={{$key}}</a> 
+</p>
